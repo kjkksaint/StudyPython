8bb3caf36204ac3371d7a5930efffc88fe9091aa
imnot.saint_ <145217533+kjkksaint@users.noreply.github.com>
2025-01-22 02:02:07 -0300

    Challenges
    Addition of some challenges I did while watching an online course from Curso em Video

---
+++ EstudyPython/desafio01.py
@@ -0,0 +1,4 @@
+print("=== DESAFIO 01 ===")
+
+nome = (input('Qual seu nome?: '))
+print ('Olá', nome, 'seja bem vindo')
---
+++ EstudyPython/desafio03.py
@@ -0,0 +1,12 @@
+print('==== desafio 03 ====')
+
+#Solocitando os dois valores:
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+
+#Calculando a soma
+soma = a + b
+
+#Exibe resultado
+print(f"A soma de {a} e {b} é igual {soma}")
